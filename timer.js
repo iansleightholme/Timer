7d203be1a8604abf2c2360ca9d9f7fbc8bb4b2cd
Ann Sleightholme <ann_sleightholme@hotmail.com>
2019-10-20 12:40:33 +0100

    Split js and css components into separate files

--- timer.js
+++ timer.js
@@ -180,28 +180,3 @@ clearInterval(timer);
 // your code goes here
 show('paused');
 }
-
-function getSettings() {
-return {
-    "version": "1.0",
-    "enabled": true,
-    "name": "Friday evening",
-    "displayName": "The Hollies Bridge Club",
-    "boardsPerRound": 6,
-    "rounds": 6,
-    "tones": false,
-    "voiceCommands": false,
-    "pause": true,
-    "pauseAfterRound": 4,
-    "boardTime": 60,
-    "overtime": 180,
-    "autoCalcOvertime": true,
-    "moveTime": "120",
-    "average": true,
-    "averageOption": "percent",
-    "averagePercent": 80,
-    "averageSeconds": 309 
-    };
-}
-
- 
