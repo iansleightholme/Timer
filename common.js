ec87b07169dc3ae9fdafa271451910edaed5741f
Ian Sleightholme <ian.sleightholme@gmail.com>
2021-01-04 10:40:16 +0000

    Modified timer.  Unmodified settings

--- common.js
+++ common.js
@@ -16,17 +16,3 @@ function addMinutesToDate(date, minutes) {
 function addMillisecondsToDate(date, milliseconds) {
     return new Date(date.getTime() + milliseconds);
 }
- 
-function getFarewell() {
-    var hour = new Date().getHours();
-    if (hour < 6)
-        return 'Good bye';
-    else if (hour < 12)
-        return 'Good morning';
-    else if (hour < 17)
-        return 'Good afternoon';
-    else if (hour < 20)
-        return 'Good evening';
-    else
-        return 'Good night';
-}
---
+++ de/language.js
@@ -0,0 +1,15 @@
+var language = "de";
+
+function getFarewell() {
+    var hour = new Date().getHours();
+    if (hour < 6)
+        return 'Auf Wiedersehen';
+    else if (hour < 12)
+        return 'Guten Morgen';
+    else if (hour < 17)
+        return 'Guten Tag';
+    else if (hour < 20)
+        return 'Guten Abend';
+    else
+        return 'Gute Nacht';
+}
